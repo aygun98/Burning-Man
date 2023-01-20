30cfafdd3712574b351b90b0ef503ba0317026b1
Aygun <aygun.xuduyeva98@gmail.com>
2023-01-20 12:29:24 -0800

    about

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const barsbtn = document.querySelector('.fa-bars')
 const xmarkBagla = document.querySelector('.fa-xmark')
 const overlay = document.querySelector('.overlay')
@@ -10,3 +12,28 @@ barsbtn.addEventListener('click', function () {
 xmarkBagla.addEventListener('click', function () {
     overlay.classList.remove('aktiv')
 })
+
+
+// TYPING EFFECT
+const h1ElementiTypeEffect = document.getElementById('center-h1-type-effect')
+
+const text = 'BURNING MAN'
+console.log(text.length)
+
+ let i = 0
+ const speed = 150
+
+ function typeEffect() {
+    if (i < text.length) {
+        h1ElementiTypeEffect.innerText += text.charAt(i)
+        i++
+        setTimeout(typeEffect,speed)
+    }
+ }
+
+ window.addEventListener('DOMContentLoaded', typeEffect)
+
+
+
+ // AOS kitabxanasi
+AOS.init();
